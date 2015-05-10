9df78e6404fadd2d97a916f6fe1682190066bf7e
Duncan Burggraf <dcburggraf@gmail.com>
2015-05-10 10:02:41 -0700

    added command line argument for run duration

--- source/main.cpp
+++ source/main.cpp
@@ -4,14 +4,32 @@
 #include <debug.h>
 
 //namespace std
-#include <cstdlib>  //std::srand()
+#include <cstdlib>  //std::srand() std::atoi() std::exit()
 #include <ctime>    //std::time()
-#include <iostream> //std::cerr
+#include <iostream> //std::cout
 
 using namespace matrixemu;
 
-int main()
+static void usage(int argc, const char * argv[]); //calls exit(1)
+
+int main(int argc, const char *argv[])
 {
+	if (argc > 2)
+	{
+		usage(argc,argv);
+	}
+
+	int frames = 1000;
+
+	if (argc == 2)
+	{
+		frames = 40 * std::atoi(argv[1]);
+		if (!frames)
+		{
+			usage(argc,argv);
+		}
+	}
+
 	//seed the prng
 	std::srand(std::time(NULL));
 
@@ -19,10 +37,21 @@ int main()
 	Terminal::init();
 
 	Matrix m;
-	m.run(1000);
+	m.run(frames);
 
 	Terminal::end();
 
 	return 0;
+}
 
+
+static void usage(int argc, const char * argv[])
+{
+	if (argc == 2)
+	{
+		std::cout << "    \"" << argv[1] << "\" could not be interpreted as an integer.\n";
+	}
+	std::cout << "    Usage: " << argv[0] << " [approx. run time in seconds]\n";
+
+	std::exit(1);
 }
